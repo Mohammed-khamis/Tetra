13ef91c549f4ae807e4b0c5ac721ac88f10c5fdd
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-11-30 22:38:41 +0200

    login

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
 import Home from './pages/Home';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Header from './components/nav/Header';
 import RegisterComplete from './pages/auth/RegisterComplete';
 
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+
 const App = () => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged(async (user) => {
+			if (user) {
+				const idTokenResult = await user.getIdTokenResult();
+
+				dispatch({
+					type: 'LOGGED_IN_USER',
+					payload: {
+						email: user.email,
+						token: idTokenResult.token,
+					},
+				});
+			}
+		});
+		return () => unsubscribe();
+	}, []);
+
 	return (
 		<>
 			<Header />
