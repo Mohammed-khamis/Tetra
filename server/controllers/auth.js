4f7a040595f1b77b8631387bac43c1890adc94ed
tetra-shayeb <mohammednm40@gmail.com>
2020-12-02 16:41:30 +0200

    Add comments

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,29 +1,30 @@
-const User = require('./../models/user');
+const User = require("../models/user");
 
 exports.createOrUpdateUser = async (req, res) => {
-	const { name, picture, email } = req.user;
+  const { name, picture, email } = req.user;
 
-	const user = await User.findOneAndUpdate(
-		{ email },
-		{ name: email.split('@')[0], picture },
-		{ new: true },
-	);
-
-	if (user) {
-		res.json(user);
-	} else {
-		const newUser = await new User({
-			email,
-			name: email.split('@')[0],
-			picture,
-		}).save();
-		res.json(newUser);
-	}
+  const user = await User.findOneAndUpdate(
+    { email },
+    { name: email.split("@")[0], picture },
+    { new: true }
+  );
+  if (user) {
+    console.log("USER UPDATED", user);
+    res.json(user);
+  } else {
+    const newUser = await new User({
+      email,
+      name: email.split("@")[0],
+      picture,
+    }).save();
+    console.log("USER CREATED", newUser);
+    res.json(newUser);
+  }
 };
 
 exports.currentUser = async (req, res) => {
-	User.find({ email: req.user.email }).exec((err, user) => {
-		if (err) throw new Error(err);
-		res.json(user);
-	});
+  User.findOne({ email: req.user.email }).exec((err, user) => {
+    if (err) throw new Error(err);
+    res.json(user);
+  });
 };
