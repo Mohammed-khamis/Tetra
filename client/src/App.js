6361531b07b91bdb6566c9f379977d7de4437042
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-12-03 11:23:11 +0200

    handle sub update

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,61 @@
-import React, { useEffect } from "react";
-import { Switch, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import React, { useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-import Login from "./pages/auth/Login";
-import Register from "./pages/auth/Register";
-import Home from "./pages/Home";
-import Header from "./components/nav/Header";
-import RegisterComplete from "./pages/auth/RegisterComplete";
-import ForgotPassword from "./pages/auth/ForgotPassword";
-import History from "./pages/user/History";
-import UserRoute from "./components/routes/UserRoute";
-import AdminRoute from "./components/routes/AdminRoute";
-import Password from "./pages/user/Password";
-import Wishlist from "./pages/user/Wishlist";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import CategoryCreate from "./pages/admin/category/CategoryCreate";
-import CategoryUpdate from "./pages/admin/category/CategoryUpdate";
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
+import Header from './components/nav/Header';
+import RegisterComplete from './pages/auth/RegisterComplete';
+import ForgotPassword from './pages/auth/ForgotPassword';
+import History from './pages/user/History';
+import UserRoute from './components/routes/UserRoute';
+import AdminRoute from './components/routes/AdminRoute';
+import Password from './pages/user/Password';
+import Wishlist from './pages/user/Wishlist';
+import AdminDashboard from './pages/admin/AdminDashboard';
+import CategoryCreate from './pages/admin/category/CategoryCreate';
+import CategoryUpdate from './pages/admin/category/CategoryUpdate';
 import SubCreate from './pages/admin/sub/SubCreate';
+import SubUpdate from './pages/admin/sub/SubUpdate';
 
 
-import { auth } from "./firebase";
-import { useDispatch } from "react-redux";
-import { currentUser } from "./functions/auth";
+import { auth } from './firebase';
+import { useDispatch } from 'react-redux';
+import { currentUser } from './functions/auth';
 
 const App = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  // to check firebase auth state
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(async (user) => {
-      if (user) {
-        const idTokenResult = await user.getIdTokenResult();
-        console.log("user", user);
+	// to check firebase auth state
+	useEffect(() => {
+		const unsubscribe = auth.onAuthStateChanged(async (user) => {
+			if (user) {
+				const idTokenResult = await user.getIdTokenResult();
+				console.log('user', user);
 
-        currentUser(idTokenResult.token)
-          .then((res) => {
-            dispatch({
-              type: "LOGGED_IN_USER",
-              payload: {
-                name: res.data.name,
-                email: res.data.email,
-                token: idTokenResult.token,
-                role: res.data.role,
-                _id: res.data._id,
-              },
-            });
-          })
-          .catch((err) => console.log(err));
-      }
-    });
-    // cleanup
-    return () => unsubscribe();
-  }, [dispatch]);
+				currentUser(idTokenResult.token)
+					.then((res) => {
+						dispatch({
+							type: 'LOGGED_IN_USER',
+							payload: {
+								name: res.data.name,
+								email: res.data.email,
+								token: idTokenResult.token,
+								role: res.data.role,
+								_id: res.data._id,
+							},
+						});
+					})
+					.catch((err) => console.log(err));
+			}
+		});
+		// cleanup
+		return () => unsubscribe();
+	}, [dispatch]);
 
-  return (
+	return (
 		<>
 			<Header />
 			<ToastContainer />
@@ -75,6 +76,7 @@ const App = () => {
 					component={CategoryUpdate}
 				/>
 				<AdminRoute exact path="/admin/sub" component={SubCreate} />
+				<AdminRoute exact path="/admin/sub/:slug" component={SubUpdate} />
 			</Switch>
 		</>
 	);
