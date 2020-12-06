05a6a49a4b5e707c4adb6f84df835d40062062a4
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-12-06 09:15:05 +0200

    create a new arrivals and best sellers components

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,17 +1,26 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 // middlewares
-const { authCheck, adminCheck } = require("../middlewares/auth");
+const { authCheck, adminCheck } = require('../middlewares/auth');
 
 // controller
-const { create, listAll, remove, read, update } = require("../controllers/product");
+const {
+	create,
+	listAll,
+	remove,
+	read,
+	update,
+	list,
+} = require('../controllers/product');
 
 // routes
-router.post("/product", authCheck, adminCheck, create);
-router.get("/products/:count", listAll); // products/100
-router.delete("/product/:slug", authCheck, adminCheck, remove);
-router.get("/product/:slug", read);
-router.put("/product/:slug", authCheck, adminCheck, update);
+router.post('/product', authCheck, adminCheck, create);
+router.get('/products/:count', listAll); // products/100
+router.delete('/product/:slug', authCheck, adminCheck, remove);
+router.get('/product/:slug', read);
+router.put('/product/:slug', authCheck, adminCheck, update);
+
+router.post('/products', list);
 
 module.exports = router;
